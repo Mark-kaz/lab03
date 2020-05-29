545ce8823a636fc210cbdd836ab12ed026d7f047
AntimonovM <mark.antimonov@gmail.com>
2020-05-29 23:09:55 +0400

    code: 3.2.7

--- main.cpp
+++ main.cpp
@@ -54,13 +54,22 @@ int main(int argc, char* argv[])
         CURLcode res;
         curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
         res = curl_easy_perform(curl);
+        res = CURLE_FAILED_INIT;
+            if (res != CURLE_OK)
+            {
+                cout << curl_easy_strerror(res) << endl;
+                exit(1);
+            }
         curl_easy_cleanup(curl);
     }
     return 0;
     }
     curl_global_init(CURL_GLOBAL_ALL);
+    // Ââîä äàííûõ
     Input data = read_input(cin, true);
+    // Îáðàáîòêà äàííûõ
     const auto bins = make_histogram(data);
+    // Âûâîä äàííûõ
     show_histogram_svg(bins);
     return 0;
 }
